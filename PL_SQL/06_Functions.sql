7abae9867d9ac44265cc4211b5b3ae2d9a6ff9a5
V4smin4 <yasmina.01027285708@gmail.com>
2026-08-08 11:02:30 +0300

    Update 06_Functions.sql

--- PL_SQL/06_Functions.sql
+++ PL_SQL/06_Functions.sql
@@ -1,1 +1,27 @@
+-- ==========================================
+-- Cybersecurity Training CTF Database
+-- File: 06_Functions.sql
+-- Description: Calculate total score for a user
+-- Database: Oracle
+-- ==========================================
 
+CREATE OR REPLACE FUNCTION Calculate_User_Score (
+    p_user_id IN Users.User_ID%TYPE
+)
+RETURN NUMBER
+AS
+    v_score NUMBER;
+BEGIN
+
+    SELECT NVL(SUM(Score_Awarded), 0)
+    INTO v_score
+    FROM Submissions
+    WHERE User_ID = p_user_id;
+
+    RETURN v_score;
+
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN 0;
+END Calculate_User_Score;
+/
